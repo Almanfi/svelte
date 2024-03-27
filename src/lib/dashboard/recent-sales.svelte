34a39ab6744e41d2ac5567076767c8654b9b448f
Almanfi <nassimed.dev@gmail.com>
2024-03-27 22:28:49 +0000

    _

--- src/lib/dashboard/recent-sales.svelte
+++ src/lib/dashboard/recent-sales.svelte
@@ -13,7 +13,7 @@
 		</Avatar.Root>
 		<div class="ml-4 space-y-1">
 			<p class="text-sm font-medium leading-none">Olivia Martin</p>
-			<p class="text-sm text-muted-foreground">olivia.martin@email.com</p>
+			<p class="text-sm text-muted-foreground max-[400px]:hidden">olivia.martin@email.com</p>
 		</div>
 		<div class="ml-auto font-medium">+$1,999.00</div>
 	</div>
@@ -24,7 +24,7 @@
 		</Avatar.Root>
 		<div class="ml-4 space-y-1">
 			<p class="text-sm font-medium leading-none">Jackson Lee</p>
-			<p class="text-sm text-muted-foreground">jackson.lee@email.com</p>
+			<p class="text-sm text-muted-foreground max-[400px]:hidden">jackson.lee@email.com</p>
 		</div>
 		<div class="ml-auto font-medium">+$39.00</div>
 	</div>
@@ -35,7 +35,7 @@
 		</Avatar.Root>
 		<div class="ml-4 space-y-1">
 			<p class="text-sm font-medium leading-none">Isabella Nguyen</p>
-			<p class="text-sm text-muted-foreground">isabella.nguyen@email.com</p>
+			<p class="text-sm text-muted-foreground max-[400px]:hidden">isabella.nguyen@email.com</p>
 		</div>
 		<div class="ml-auto font-medium">+$299.00</div>
 	</div>
@@ -46,7 +46,7 @@
 		</Avatar.Root>
 		<div class="ml-4 space-y-1">
 			<p class="text-sm font-medium leading-none">William Kim</p>
-			<p class="text-sm text-muted-foreground">will@email.com</p>
+			<p class="text-sm text-muted-foreground max-[400px]:hidden">will@email.com</p>
 		</div>
 		<div class="ml-auto font-medium">+$99.00</div>
 	</div>
@@ -57,7 +57,7 @@
 		</Avatar.Root>
 		<div class="ml-4 space-y-1">
 			<p class="text-sm font-medium leading-none">Sofia Davis</p>
-			<p class="text-sm text-muted-foreground">sofia.davis@email.com</p>
+			<p class="text-sm text-muted-foreground max-[400px]:hidden">sofia.davis@email.com</p>
 		</div>
 		<div class="ml-auto font-medium">+$39.00</div>
 	</div>
