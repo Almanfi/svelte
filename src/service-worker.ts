d4411dc5ad46118e57e19ca4601f823ae9169dcd
Almanfi <nassimed.dev@gmail.com>
2024-04-13 23:11:00 +0000

    disable fetcing cash in service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -39,42 +39,46 @@ self.addEventListener('activate', (event) => {
     }
 });
 
-self.addEventListener('fetch', (event) => {
-    try{
-
-        if (event.request.method !== 'GET') return;
-        
-        async function respond() {
-            const url = new URL(event.request.url);
-            const cache = await caches.open(CACHE);
-            
-            if (ASSETS.includes(url.pathname)) {
-                const cachedResponse = await cache.match(url.pathname);
-            if (cachedResponse) return cachedResponse;
-        }
-        
-        try {
-            const response = await fetch(event.request);
-            const isNotExtension = url.protocol === 'http:' || url.protocol === 'https:';
-            const isSuccessful = response.status === 200;
-            
-            if (isNotExtension && isSuccessful) {
-                cache.put(event.request, response.clone());
-            }
-            return response;
-        }
-        catch {
-            const cachedResponse = await cache.match(url.pathname);
-            if (cachedResponse) return cachedResponse;
-        }
-        return new Response('Not found', { status: 404 });
-    }
-    event.respondWith(respond());
-    }
-    catch(err){
-        console.error(err)
-    }
+self.addEventListener('fetch', function () {
+	return;
 });
+
+// self.addEventListener('fetch', (event) => {
+//     try{
+
+//         if (event.request.method !== 'GET') return;
+        
+//         async function respond() {
+//             const url = new URL(event.request.url);
+//             const cache = await caches.open(CACHE);
+            
+//             if (ASSETS.includes(url.pathname)) {
+//                 const cachedResponse = await cache.match(url.pathname);
+//             if (cachedResponse) return cachedResponse;
+//         }
+        
+//         try {
+//             const response = await fetch(event.request);
+//             const isNotExtension = url.protocol === 'http:' || url.protocol === 'https:';
+//             const isSuccessful = response.status === 200;
+            
+//             if (isNotExtension && isSuccessful) {
+//                 cache.put(event.request, response.clone());
+//             }
+//             return response;
+//         }
+//         catch {
+//             const cachedResponse = await cache.match(url.pathname);
+//             if (cachedResponse) return cachedResponse;
+//         }
+//         return new Response('Not found', { status: 404 });
+//     }
+//     event.respondWith(respond());
+//     }
+//     catch(err){
+//         console.error(err)
+//     }
+// });
 
 self.addEventListener('push', function (event: any) {
 	const payload = event.data?.text() ?? 'no payload';
